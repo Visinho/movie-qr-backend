392fd8c219752159da176cee75d3d2cb3e829f77
Visinho <Elvisvisinho@rocketmail.com>
2023-10-12 11:08:38 +0100

    Created the module and controller files for the movies

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+// import { AppController } from './app.controller';
+// import { AppService } from './app.service';
+import { MoviesModule} from './movies/movies.module';
+import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ScheduleModule.forRoot(), MoviesModule],
+  // controllers: [AppController],
+  // providers: [AppService],
 })
 export class AppModule {}
