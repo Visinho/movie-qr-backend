63de02102d4d6206a9a53bc491851976d75f74d4
Visinho <Elvisvisinho@rocketmail.com>
2023-10-14 09:18:09 +0100

    Changes to the main file

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,11 @@
+// Clear Node.js module cache
+Object.keys(require.cache).forEach(function(key) {
+  delete require.cache[key];
+});
+
+// Your application code
+import * as app from './main';
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
